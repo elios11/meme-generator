9fc9f8ee41198af0010a83092909ad7f52555a03
elios11 <eliosferre3@hotmail.com>
2022-12-06 20:03:00 -0300

    Initialize React project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="/troll-face.png" />
+            <h2 className="header--title">Meme Generator</h2>
+            <p className="header--subtitle">React Course - Project 3</p>
+        </header>
+    )
+}
