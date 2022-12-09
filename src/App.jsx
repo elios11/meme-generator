d340399110e041be5281b39e93d4a3d0d80412e7
elios11 <eliosferre3@hotmail.com>
2022-12-09 04:13:12 -0300

    Import and render Meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header"
+import Meme from "./components/Meme/Meme"
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Meme />
         </div>
     )
 }
